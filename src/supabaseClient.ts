c5f5aa04724ba69e5b9dcf167285dc26e27cfd32
AleAlvo <a.borges.alvaro@gmail.com>
2024-07-31 17:16:47 +0100

    minor changes

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,11 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log('SUPABASE_URL:', process.env.SUPABASE_URL)
-console.log('SUPABASE_KEY:', process.env.SUPABASE_KEY)
-
 // Retrieve environment variables
 const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseKey = process.env.SUPABASE_KEY || ''
+const supabaseKey = process.env.SUPABASE_PUBLIC_KEY || ''
 
 // Initialize Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey)
